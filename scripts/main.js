75d885f744517a872d1c0faac82d0c007ad57247
agnescameron <agnesfcameron@gmail.com>
2020-03-07 10:36:56 -0500

    committing extension

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+$('img').attr({'src': 'https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_200a0224-6790-4f7a-8088-ed32fd21dad0.png'});
+
+
+
+$('div').css({
+	"background-color": "yellow",
+	"font-family": "arial black"
+})
