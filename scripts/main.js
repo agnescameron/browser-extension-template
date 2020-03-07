6f37e6994e8a47f537bbfb4da116346b4f5c32e2
agnescameron <agnesfcameron@gmail.com>
2020-03-07 10:55:39 -0500

    potatoes on the new york times

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,2 @@
-$('img').attr({'src': 'https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_200a0224-6790-4f7a-8088-ed32fd21dad0.png', 'srcset': 'https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_200a0224-6790-4f7a-8088-ed32fd21dad0.png'});
-
-
-
-$('div').css({
-	"background-color": "yellow",
-	"font-family": "arial black"
-})
+$('img').attr({'src': 'https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_200a0224-6790-4f7a-8088-ed32fd21dad0.png', 
+	'srcset': 'https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_200a0224-6790-4f7a-8088-ed32fd21dad0.png'});
